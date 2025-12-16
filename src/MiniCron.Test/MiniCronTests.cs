93d638abe1edd585d3a2810e7c73c7bbcbe7fc3c
Jean LAURENT <jeanz.laurent@gmail.com>
2025-12-16 19:06:02 +0100

    test(tests): Add unit test for AddMiniCron method in ServiceCollectionExtensions - MiniCronTests.cs

--- src/MiniCron.Test/MiniCronTests.cs
+++ src/MiniCron.Test/MiniCronTests.cs
@@ -1,10 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using MiniCron.Core.Extensions;
+using MiniCron.Core.Services;
+
 namespace MiniCron.Test;
 
 public class MiniCronTests
 {
     [Fact]
-    public void Test1()
+    public void ServiceCollectionExtensions_AddMiniCron_DefaultOptions()
     {
-        
+        var services = new ServiceCollection();
+        services.AddMiniCron(options => { /* No specific options for this test */ });
+        Assert.Contains(services, sd => sd.ServiceType == typeof(JobRegistry));
     }
 }
